79dfbf99a0065a03f999092d2e3394e3da26ada2
gaidardzhiev <1v4n@protonmail.com>
2025-01-14 11:06:17 +0200

    Tue Jan 14 09:06:16 AM UTC 2025

--- auto.sh
+++ auto.sh
@@ -2,10 +2,19 @@
 
 set -x
 
-if termux-camera-photo /sdcard/image.jpg; then
-	./ocr /sdcard/image.jpg
-	exit 0
-else
-	printf "something's wrong in here somewhere...\n"
-	exit 1
-fi
+#if termux-camera-photo /sdcard/image.jpg; then
+#	if ./ocr /sdcard/image.jpg ; then
+#		rm /sdcard/image.jpg
+#		exit 0
+#	else
+#		printf "something's wrong in here somewhere...\n"
+#		exit 2
+#	fi
+#else
+#	printf "something's wrong in here somewhere...\n"
+#	exit 1
+#fi
+
+termux-camera-photo /sdcard/image.jpg && ./ocr /sdcard/image.jpg && rm /sdcard/image.jpg && exit 0
+
+printf "something's wrong in here somewhere...\n" && exit 1
